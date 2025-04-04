4924b365661222ca94dca3a775c9d08f709444b9
Caleb Karimi <karimicaleb@gmail.com>
2025-04-04 22:02:15 +0300

    Create Image Detector files

---
+++ src/components/ImageDetector.js
@@ -0,0 +1,9 @@
+"use client";
+
+import '../styles/ImageDetector.css';
+
+export default function ImageDetector() {
+    return (
+        <div className="Image-detector-component"></div>
+    );
+}
